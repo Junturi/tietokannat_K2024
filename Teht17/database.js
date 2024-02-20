0655164c2ec69786e94884aa679e713a6293ee40
Junturi <155945430+Junturi@users.noreply.github.com>
2024-02-20 14:00:14 +0200

    Delete Teht17 directory
    Korjaus tehtävään

--- Teht17/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'test_user',
-  password: 'testi',
-  database: 'test_db2'
-});
-module.exports = connection;
